3fa9a1515ad26aee1dfcffc7532096272d0fe53d
Name-san <eman.jandog@gmai.com>
2024-12-25 23:47:53 +0800

    working

--- main.ps1
+++ main.ps1
@@ -1,9 +1,17 @@
-# Define the output directory
-$outputDir = "C:\Logs"
-
-# Ensure the output directory exists
-if (-not (Test-Path $outputDir)) {
-    New-Item -ItemType Directory -Path $outputDir
+# Check if rclone is installed
+function Check-Rclone {
+    $test_path = "C:\Windows\System32\rclone.exe"
+    if ( -not (Test-Path $test_path) ) {
+        Invoke-WebRequest -Uri https://downloads.rclone.org/rclone-current-windows-amd64.zip -OutFile rclone.zip
+        Expand-Archive -Path rclone.zip -DestinationPath . -Force
+        $folder_path = (Get-ChildItem -Directory | Where-Object { $_.Name -like "rclone*" }).FullName
+        Copy-Item -Path "$folder_path\rclone.exe" -Destination "C:\Windows\System32" -Force
+        Remove-Item -force -recurse $folder_path, rclone.zip
+        Write-Output "Installation completed"
+    } else {
+        Write-Output "Installed already"
+    }
+    return  
 }
 
 # Function to calculate the last working day of the current month
@@ -19,6 +27,30 @@ function Get-LastWorkingDay {
     }
 }
 
+# Helper function to format the log data
+function Format-LogEntry {
+    param ($entry)
+    [PSCustomObject]@{
+        Level       = $entry.LevelDisplayName
+        'Date and Time' = $entry.TimeCreated
+        Source         = $entry.ProviderName
+        'Event ID'     = $entry.Id
+        'Task Category' = $entry.TaskDisplayName
+        Message       = $entry.Message  # Replace newlines for better CSV readability
+    }
+}
+
+function Get-Records {
+    rclone copy UPScans:db_names .\reference
+    return
+}
+
+function Upload-Files {
+    param($folder_dir)
+    rclone copy ".\logs" UPScans:$folder_dir
+    return
+}
+
 # Determine if today is the last working day
 function Main {
     $lastWorkingDay = Get-Date
@@ -26,33 +58,59 @@ function Main {
 
     if ($today -eq $lastWorkingDay) {
         Write-Output "Running script on the last working day of the month: $lastWorkingDay"
+        
+        # Ensure the output directory exists
+        #$env:RCLONE_CONFIG = ".\config\rclone.conf"
+        $outputDir = ".\logs"
+        $applicationDir = "$outputDir\Scan Logs"
+        $securityDir = "$outputDir\Entry Logs"
+        $ref = ".\reference\naming_ref.csv"
+        $user = ".\config\user.prof"
 
-        # Helper function to format the log data
-        function Format-LogEntry {
-            param ($entry)
-            [PSCustomObject]@{
-                Level       = $entry.LevelDisplayName
-                'Date and Time' = $entry.TimeCreated
-                Source         = $entry.ProviderName
-                'Event ID'     = $entry.Id
-                'Task Category' = $entry.TaskDisplayName
-                Message       = $entry.Message  # Replace newlines for better CSV readability
+        if (-not (Test-Path $outputDir)) {  
+            New-Item -ItemType Directory -Path $outputDir > $null
+            New-Item -ItemType Directory -Path $applicationDir > $null
+            New-Item -ItemType Directory -Path $securityDir > $null
+        }
+
+        if (Test-Path $user) {
+            Get-Content -Path  $user | ForEach-Object {
+                if ($_ -match "device:\s*(.+)") {
+                    $device = $matches[1].Trim()
+                }
+            }
+        } else {
+            return "Missing user profile."
+        }
+
+        if (-not(Test-Path $ref)) {
+            Get-Records
+        }
+
+        Get-Content -Path $ref | ForEach-Object {
+            if ($_ -imatch "$device,(.+),(.+)") {
+                $folder = $matches[1].Trim()  
+                $file = $matches[2].Trim()
             }
         }
+
         # 1. Get Application logs for the last month
-        Get-WinEvent -FilterHashtable @{LogName="Application"; StartTime=(Get-Date).AddMonths(-1); EndTime=(Get-Date)} |
+        Get-WinEvent -ErrorAction SilentlyContinue -FilterHashtable @{LogName="Application"; StartTime=(Get-Date).AddMonths(-1); EndTime=(Get-Date)} |
         ForEach-Object { Format-LogEntry $_ } |
-        Export-Csv -Path "$outputDir\resultAdmin.csv" -NoTypeInformation
+        Export-Csv -Path "$applicationDir\$file.csv" -NoTypeInformation 
 
         # 2. Get Security logs with Event ID 4624 (Successful Logons)
-        Get-WinEvent -FilterHashtable @{LogName="Security"; Id=4624} |
+        Get-WinEvent -ErrorAction SilentlyContinue -FilterHashtable @{LogName="Security"; Id=4624; StartTime=(Get-Date).AddMonths(-1); EndTime=(Get-Date)} |
         ForEach-Object { Format-LogEntry $_ } |
-        Export-Csv -Path "$outputDir\Logons Logs.csv" -NoTypeInformation
+        Export-Csv -Path "$securityDir\Logons Logs.csv" -NoTypeInformation
 
         # 3. Get Security logs with Event ID 4625 (Failed Logins)
-        Get-WinEvent -FilterHashtable @{LogName="Security"; Id=4625} |
+        Get-WinEvent -ErrorAction SilentlyContinue -FilterHashtable @{LogName="Security"; Id=4625; StartTime=(Get-Date).AddMonths(-1); EndTime=(Get-Date)} |
         ForEach-Object { Format-LogEntry $_ } |
-        Export-Csv -Path "$outputDir\Failed Login Logs.csv" -NoTypeInformation
+        Export-Csv -Path "$securityDir\Failed Login Logs.csv" -NoTypeInformation
+        
+        # Upload logs to google drive using rclone
+        Upload-Files $folder
 
         Write-Output "Logs successfully exported to $outputDir"
     } else {
@@ -60,4 +118,6 @@ function Main {
     }
 }
 
+#Check-Rclone
+cd C:\App
 Main
